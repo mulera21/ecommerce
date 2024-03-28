853de95aa1c38675e877cf789562f6f2c58a5e09
edmond <edmondmulera55@gmail.com>
2024-03-28 16:24:30 +0300

    added cart page

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    User = models.OneToOneField(User,null=True, blank=True)
